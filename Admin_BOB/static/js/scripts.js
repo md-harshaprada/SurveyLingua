b84640d5c63c7a0dfd7c5b96486bd970aa377c46
SriSailaAS <96300556+SriSailaAS@users.noreply.github.com>
2023-03-14 20:40:55 +0530

    Admin page

---
+++ Admin_BOB/static/js/scripts.js
@@ -0,0 +1,47 @@
+$("form[name=signup_form").submit(function(e) {
+
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+    
+  
+    $.ajax({
+      url: "/user/signup",
+      type: "POST",
+      data: data,
+      dataType: "json",
+      success: function(resp) {
+        window.location.href = "/dashboard/";
+      },
+      error: function(resp) {
+        $error.text(resp.responseJSON.error).removeClass("error--hidden");
+      }
+    });
+  
+    e.preventDefault();
+  });
+  
+  $("form[name=login_form").submit(function(e) {
+  
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+    $.ajax({
+      url: "/user/login",
+      type: "POST",
+      data: data,
+      dataType: "json",
+      success: function(resp) {
+        if (resp.email === "srisailaas@gmail.com") {
+          window.location.href = "/dashboard/";
+        } else {
+          window.location.href = "/";
+        }
+      },
+      error: function(resp) {
+        $error.text(resp.responseJSON.error).removeClass("error--hidden");
+      }
+    });
+  
+    e.preventDefault();
+  });
